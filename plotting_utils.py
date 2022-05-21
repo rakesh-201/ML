106e8402177f589d808a918f04a9b1a7a3fc7556
Rakesh Rajpurohit <60790259+rakesh-201@users.noreply.github.com>
2022-05-21 10:49:07 +0530

    first commit

---
+++ plotting_utils.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+def plot_1(preds_list, batt_no, tr_labels):
+    files = ['MSVR','RSVR','PSVR']
+    c = ['b','c','r']
+    fig=plt.figure()
+    for x, col, file in zip(preds_list[:-1], c, files):
+        s = np.add(np.arange(len(x)), (len(tr_labels)-len(preds_list[-1])) * np.ones(len(x)))
+        plt.plot(s, x, color = col, label = file )
+    plt.plot(np.arange(len(tr_labels)), tr_labels, color= 'k', label= 'original')
+    plt.legend(loc='upper right', frameon=False)
+    plt.figure(figsize=(300,300))
+    #C:\Users\USER\Downloads\Li-RUL-master\Li-RUL-master\figs
+    fig.savefig('C:/Users/USER/Downloads/Li-RUL-master/Li-RUL-master/figs/1_'+str(batt_no)+'.jpg')
+
+def plot_2(preds_list, batt_no, tr_labels):
+    files = ['MSVR','RSVR']
+    c = ['b','r']
+    fig=plt.figure()
+    for x, col, file in zip(preds_list[:-1], c, files):
+        s = np.add(np.arange(len(x)), (len(tr_labels)-len(preds_list[-1])) * np.ones(len(x)))
+        plt.plot(s, x, color = col, label = file )
+    plt.plot(np.arange(len(tr_labels)), tr_labels, color= 'k', label= 'original')
+    plt.legend(loc='upper right', frameon=False)
+    plt.figure(figsize=(300,300))
+    fig.savefig('C:/Users/USER/Downloads/Li-RUL-master/Li-RUL-master/figs/2_'+str(batt_no)+'.jpg')
